edfb3af32acbd28f0f59a7185bf2613ff87a0e39
Satwinder <satwinder.singh@telenor.se>
2022-04-10 22:04:28 +0200

    Added animation on the cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,7 +11,7 @@ const Cart = (props) => {
   const hasItems = cartCtx.items.length > 0;
 
   const cartItemRemovalHandler = (id) => {
-
+    cartCtx.removeItem(id);
   };
 
   const cartItemAddHandler = (item) => {
